ec9533d693cdb9dd76151bc3a074cf224e63c782
Vinicius Ventura <vinicius__20@hotmail.com>
2022-04-16 23:57:39 -0300

    feat: adicionando componentes header e footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import { Header } from "@/components"
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -12,6 +14,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <h1 className="text-3xl font-bold underline">Hello World</h1>
 
     </div>
